1714c3d9a17a4f923cba548cb27865df77eae9ec
pwwang <pwwang@pwwang.com>
2019-08-05 18:22:31 -0500

    Add tests

--- pyppl-flowchart/__init__.py
+++ pyppl-flowchart/__init__.py
@@ -3,52 +3,65 @@
 import sys
 from pathlib import Path
 from pyppl.plugin import hookimpl, prerun, addmethod
-from pyppl.logger import logger
+from pyppl.logger import logger, THEMES, LEVELS_ALWAYS
+from pyppl.proctree import ProcTree
 from .flowchart import Flowchart
 
 __version__ = "0.0.1"
+
+LOGGER_THEME = dict(
+	greenOnBlack   = dict(FCHART = '{f.GREEN}'),
+	blueOnBlack    = dict(FCHART = '{f.BLUE}'),
+	magentaOnBlack = dict(FCHART = '{f.MAGENTA}'),
+	greenOnWhite   = dict(FCHART = '{f.GREEN}'),
+	blueOnWhite    = dict(FCHART = '{f.BLUE}'),
+	magentaOnWhite = dict(FCHART = '{f.MAGENTA}'),
+)
+
+class ProcHideError(Exception):
+	"""Raise when a process cannot be hidden in the flowchart"""
 
 @hookimpl
 def setup(config):
 	"""
 	Setup the plugin
 	"""
+	for theme, colors in THEMES.items():
+		colors.update(LOGGER_THEME.get(theme, {}))
+	LEVELS_ALWAYS.add('FCHART')
 	config['hide'] = False
 	config['_flowchart'] = dict(theme = 'default')
 
-def pypplAllroutes(ppl):
-	"""@API
-	Show all the routes in the log.
-	@returns:
-		(PyPPL): The pipeline object itppl.
+def allPathsWithoutHidden(allpaths):
+	"""
+	Get all paths without hidden processes
 	"""
 	logger.debug('ALL ROUTES:')
-	#paths  = sorted([list(reversed(path)) for path in ppl.tree.getAllPaths()])
-	paths  = sorted([pnode.name() for pnode in reversed(apath)]
-		for apath in ppl.tree.getAllPaths())
-	paths2 = [] # processes merged from the same procset
-	for apath in paths:
-		prevset = None
-		path2    = []
-		for pnode in apath:
-			if not '@' in pnode:
-				path2.append(pnode)
-			else:
-				procset = pnode.split('@')[-1]
-				if not prevset or prevset != procset:
-					path2.append('[%s]' % procset)
-					prevset = procset
-				elif prevset == procset:
-					continue
-		if path2 not in paths2:
-			# pylint: disable=invalid-name
-			paths2.append(path2)
-		# see details for procset
-		#if path != path2:
-		#	logger.logger.info('[  DEBUG] * %s' % (' -> '.join(path)))
-
-	for path2 in paths2:
-		logger.debug('* %s', ' -> '.join(path2))
+	ret = []
+	for apath in allpaths:
+		# end -> start
+		# check starts and ends, they cannot hide
+		if apath[0].hide:
+			raise ProcHideError(apath[0].name() + ': end process cannot hide.')
+		if apath[-1].hide:
+			raise ProcHideError(apath[-1].name() + ': start process cannot hide.')
+		hidden = [proc for proc in apath if proc.hide]
+		for hid in hidden:
+			node = ProcTree.NODES[hid]
+			if len(node.prev) > 1 and len(node.next) > 1:
+				raise ProcHideError(hid.name() + ': cannot be hidden in flowchart as it has both more than 1 parents and offsprings.')
+			apath.remove(hid)
+		if apath not in ret:
+			# merge procset
+			paths = []
+			for proc in reversed(apath):
+				if not proc.procset:
+					paths.append(proc.name())
+				elif not paths or '[@' + proc.procset + ']' != paths[-1]:
+					paths.append('[@' + proc.procset + ']')
+			logger.debug('  %s', ' -> '.join(paths))
+			ret.append(apath)
+	return ret
 
 @prerun
 def pypplFlowchart (ppl, fcfile = None, dotfile = None):
@@ -63,29 +76,26 @@ def pypplFlowchart (ppl, fcfile = None, dotfile = None):
 	@returns:
 		(PyPPL): The pipeline object itself.
 	"""
-	pypplAllroutes(ppl)
+	allpaths = allPathsWithoutHidden(ppl.tree.getAllPaths())
 	fcfile  = fcfile or (Path('.') / Path(sys.argv[0]).stem).with_suffix(
 		'%s.pyppl.svg' % ('.' + str(ppl.counter) if ppl.counter else ''))
 	dotfile = dotfile if dotfile else Path(fcfile).with_suffix('.dot')
 	fchart  = Flowchart(fcfile = fcfile, dotfile = dotfile)
 	fchart.setTheme(ppl.config._flowchart.theme)
 
-	for start in ppl.tree.getStarts():
-		fchart.addNode(start, 'start')
-	for end in ppl.tree.getEnds():
-		fchart.addNode(end, 'end')
-		for apath in ppl.tree.getPathsToStarts(end):
-			for i, pnode in enumerate(apath):
-				if i == 0:
-					fchart.addNode(pnode)
-					fchart.addLink(pnode, end)
-				else:
-					fchart.addNode(pnode)
-					fchart.addLink(pnode, apath[i-1])
+	for apath in allpaths:
+		fchart.addNode(apath[0], 'end')
+		fchart.addNode(apath[-1], 'start')
+		for i, proc in enumerate(apath):
+			if i == 0:
+				continue
+			if i < len(apath) - 1:
+				fchart.addNode(proc)
+			fchart.addLink(proc, apath[i-1])
 
 	fchart.generate()
-	logger.info ('Flowchart file saved to: %s', fchart.fcfile)
-	logger.info ('DOT file saved to: %s', fchart.dotfile)
+	logger.fchart('Flowchart file saved to: %s', fchart.fcfile)
+	logger.fchart('DOT file saved to: %s', fchart.dotfile)
 
 @hookimpl
 def pypplInit(ppl):
--- tests/test__init.py
+++ tests/test__init.py
@@ -1,7 +1,13 @@
 
 ## test __init__.py
+import sys
 import pytest
-from pyppl import PyPPL, Proc, Box, ProcSet
+from os import environ
+from pathlib import Path
+environ['PYPPL_default__log'] = 'py:{"leveldiffs": "DEBUG"}'
+from pyppl import PyPPL, Proc, Box, ProcSet, ProcTree
+from pyppl.utils import fs
+pyppl_flowchart = __import__('pyppl-flowchart')
 
 # don't use fix_flowchart, as plugin won't be loaded
 @pytest.fixture
@@ -10,19 +16,106 @@ def procs():
 		p1 = Proc('p1'),
 		p2 = Proc('p2'),
 		p3 = Proc('p3'),
-		p4 = Proc('p4'),
+		p4 = Proc('p4')
 	)
 
 @pytest.fixture
 def procset(procs):
 	return ProcSet(procs.p1, procs.p2, procs.p3, procs.p4)
 
+@pytest.fixture
+def pset():
+	ProcTree.NODES.clear()
+	p14 = Proc()
+	p15 = Proc()
+	p16 = Proc()
+	p17 = Proc()
+	p18 = Proc()
+	p19 = Proc()
+	p20 = Proc()
+	# p15 -> p16  ->  p17 -> 19
+	# p14 _/  \_ p18_/  \_ p20
+	#           hide
+	p18.hide = True
+	p20.depends = p17
+	p19.depends = p17
+	p17.depends = p16, p18
+	p18.depends = p16
+	p16.depends = p14, p15
+	return Box(p15 = p15, p16 = p16, p17 = p17, p18 = p18, p19 = p19, p20 = p20, p14 = p14)
 
 def test_pyppl_flowchart_init(procs, tmp_path):
 	fcfile = tmp_path / 'test_pyppl_flowchart_init.svg'
+	procs.p2.depends = procs.p1
+	procs.p3.depends = procs.p2
+	procs.p4.depends = procs.p3
 	PyPPL().start(procs.p1).flowchart(fcfile).run()
 
 def test_procset(procset, tmp_path):
 	fcfile = tmp_path / 'test_procset.svg'
-	PyPPL().start(procset).flowchart().run()
+	PyPPL().start(procset).flowchart(fcfile).run()
 
+
+def test_showallroutes(pset, caplog, tmp_path):
+	fcfile = tmp_path / 'test_showallroutes.svg'
+	# p15 -> p16  ->  ps -> p19
+	# p14 _/  \_ p18_/  \_ p20
+	#           hide
+	pset.p17.depends = []
+	ps = ProcSet(Proc(id = 'p1'), Proc(id = 'p2'))
+	ps.depends = pset.p16, pset.p18
+	pset.p19.depends = ps
+	pset.p20.depends = ps
+	ppl = PyPPL().start(pset.p14, pset.p15)
+	ppl.flowchart(fcfile)
+	assert 'ALL ROUTES:' in caplog.text
+	assert '  p14 -> p16 -> [@ps] -> p19' in caplog.text
+	assert '  p14 -> p16 -> [@ps] -> p20' in caplog.text
+	assert '  p15 -> p16 -> [@ps] -> p19' in caplog.text
+	assert '  p15 -> p16 -> [@ps] -> p20' in caplog.text
+
+def test_flowchart(pset, caplog, tmp_path):
+	for p in pset.values():
+		p.input = {'a': [1]}
+		p.output = 'a:var:{{i.a}}'
+	ppl = PyPPL({'ppldir': tmp_path / 'test_flowchart_ppldir'}).start(pset.p14, pset.p15)
+	ppl.counter = 0
+	ppl.flowchart()
+	assert 'Flowchart file saved to:' in caplog.text
+	assert 'DOT file saved to:' in caplog.text
+	assert fs.exists('./%s.pyppl.svg' % Path(sys.argv[0]).stem)
+	assert fs.exists('./%s.pyppl.dot' % Path(sys.argv[0]).stem)
+
+	dot = Path('./%s.pyppl.dot' % Path(sys.argv[0]).stem).read_text()
+	assert 'p17 -> p19' in dot
+	assert 'p17 -> p20' in dot
+	assert 'p16 -> p17' in dot
+	#assert 'p16 -> p18' in dot # hidden
+	assert 'p14 -> p16' in dot
+	assert 'p15 -> p16' in dot
+
+	fs.remove('./%s.pyppl.svg' % Path(sys.argv[0]).stem)
+	fs.remove('./%s.pyppl.dot' % Path(sys.argv[0]).stem)
+
+def test_hideerror(procs, tmp_path):
+	procs.p1.hide = True
+	procs.p2.depends = procs.p1
+	procs.p3.depends = procs.p2
+	procs.p4.depends = procs.p3
+
+	with pytest.raises(pyppl_flowchart.ProcHideError):
+		PyPPL().start(procs.p1).flowchart(tmp_path / 'test_hideerror1.svg')
+
+	procs.p1.hide = False
+	procs.p4.hide = True
+	with pytest.raises(pyppl_flowchart.ProcHideError):
+		PyPPL().start(procs.p1).flowchart(tmp_path / 'test_hideerror2.svg')
+
+	p5 = Proc()
+	procs.p4.hide = False
+	procs.p2.depends = procs.p1, p5
+	procs.p4.depends = procs.p2
+	procs.p3.depends = procs.p2
+	procs.p2.hide = True
+	with pytest.raises(pyppl_flowchart.ProcHideError):
+		PyPPL().start(procs.p1, p5).flowchart(tmp_path / 'test_hideerror3.svg')
